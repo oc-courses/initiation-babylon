f430924aa614173209d7c96ebc78bcb848dcf170
Valistarias <mallet.victor.france@gmail.com>
2016-10-09 21:37:19 +0200

    Initial Comit

--- js/Arena.js
+++ js/Arena.js
@@ -4,14 +4,46 @@ Arena = function(game) {
     var scene = game.scene;
 
     // Création de notre lumière principale
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 20, 0), scene);
 
-    // Créons une sphère 
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+    // Material pour le sol
+    var materialGround = new BABYLON.StandardMaterial("wallTexture", scene);
+    materialGround.diffuseTexture = new BABYLON.Texture("assets/images/brick.jpg", scene);
+    materialGround.diffuseTexture.uScale = 4.0;
+    materialGround.diffuseTexture.vScale = 4.0;
 
-    // Remontons le sur l'axe y de la moitié de sa hauteur
-    sphere.position.y = 1;
+    // Material pour les objets
+    var materialWall = new BABYLON.StandardMaterial("groundTexture", scene);
+    materialWall.diffuseTexture = new BABYLON.Texture("assets/images/wood.jpg", scene);
 
-    // Ajoutons un sol pour situer la sphere dans l'espace
-    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+    // Ajoutons un sol pour situer la sphère dans l'espace
+    var ground = BABYLON.Mesh.CreateGround("ground1", 20, 20, 2, scene);
+    ground.scaling = new BABYLON.Vector3(2,10,3);
+    ground.scaling.z = 2;
+    ground.material = materialGround;
+
+    // SUR TOUS LES AXES Y -> On monte les meshes de la moitié de la hauteur du mesh en question.
+    var mainBox = BABYLON.Mesh.CreateBox("box1", 3, scene);
+    mainBox.scaling.y = 1;
+    mainBox.position = new BABYLON.Vector3(5,((3/2)*mainBox.scaling.y),5);
+    // Vu que cet objet sert de clone pour les autres, les deux valeurs ci-dessous sont clonées
+    mainBox.rotation.y = (Math.PI*45)/180;
+    mainBox.material = materialWall;
+
+    var mainBox2 = mainBox.clone("box2");
+    mainBox2.scaling.y = 2;
+    mainBox2.position = new BABYLON.Vector3(5,((3/2)*mainBox2.scaling.y),-5);
+
+    var mainBox3 = mainBox.clone("box3");
+    mainBox3.scaling.y = 3;
+    mainBox3.position = new BABYLON.Vector3(-5,((3/2)*mainBox3.scaling.y),-5);
+
+    var mainBox4 = mainBox.clone("box4");
+    mainBox4.scaling.y = 4;
+    mainBox4.position = new BABYLON.Vector3(-5,((3/2)*mainBox4.scaling.y),5);
+
+    var cylinder = BABYLON.Mesh.CreateCylinder("cyl1", 20, 5, 5, 20, 4, scene);
+    cylinder.position.y = 20/2;
+    cylinder.material = materialWall;
+
 };
